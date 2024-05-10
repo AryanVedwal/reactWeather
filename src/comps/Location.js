658a9ed3ff609aba390b3e4d3a7f188067515c1a
AryanVedwal <aryanvedwal01@gmail.com>
2024-05-10 09:40:48 +0530

    minor changes

--- src/comps/Location.js
+++ src/comps/Location.js
@@ -19,16 +19,14 @@ export default function Location({ onSearch, weather, bg }) {
   return (
     <div className="lg:h-screen lg:w-[65%] w-full flex flex-col items-center justify-center gap-1 lg:p-2">
       {/* Location */}
-      <div
-        className="w-full min-h-[70vh] lg:h-[70vh] lg:rounded-xl flex flex-col justify-start gap-4 lg:gap-0 lg:justify-between p-4"
-        style={{
-          backgroundImage: `linear-gradient(90deg, #33333340, #33333340), url(${bg()})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <div className="flex flex-col gap-4">
+      <div className=" bg-[#20202020] w-full min-h-[70vh] lg:h-[70vh] lg:rounded-xl flex flex-col justify-start gap-4 lg:gap-0 lg:justify-between p-4 relative overflow-hidden">
+        <img
+          src={`${bg()}`}
+          className="object-cover absolute inset-0 z-10 opacity-80"
+          alt=""
+          loading="lazy"
+        />
+        <div className="flex flex-col gap-4 relative z-20">
           <div className="flex justify-center sm:justify-start h-[50px] sm:h-[70px]">
             <input
               type="text"
@@ -50,15 +48,14 @@ export default function Location({ onSearch, weather, bg }) {
           </div>
 
           {weather && weather.city && (
-            <h1 className="p-1 flex items-center gap-[1ch] justify-center lg:justify-start">
+            <h1 className="p-1 flex items-center gap-[1ch] justify-center lg:justify-start relative z-20">
               <Map /> {weather.city.name}, {weather.city.country}
             </h1>
           )}
         </div>
-
         {weather && weather.list && (
           <div
-            className="flex flex-col gap-4 py-2 px-4 bg-[#ffffff05] "
+            className="flex flex-col gap-4 py-2 px-4 bg-[#ffffff05] z-20"
             style={{
               backdropFilter: "blur(2.5px)",
             }}
@@ -105,9 +102,8 @@ export default function Location({ onSearch, weather, bg }) {
             </div>
           </div>
         )}
-
         {!weather.list && (
-          <div className="flex items-center justify-center p-8 h-full w-full">
+          <div className="flex items-center justify-center p-8 h-full w-full relative z-20">
             No Search Made Yet
           </div>
         )}
